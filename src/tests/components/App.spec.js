6121087ba1fe17ba8e3f81fa5887c8e796a9e8d4
fiiwebarry <fiiwebarry@yahoo.com>
2024-03-01 13:10:11 +0100

    vitest-configuration-reactlib-installations

---
+++ src/tests/components/App.spec.js
@@ -0,0 +1,7 @@
+import { describe, it, expect } from "vitest";
+
+describe("a simple text coverage", () => {
+  it("should display a text on my page", () => {
+    expect(true).toBeTruthy();
+  });
+});
